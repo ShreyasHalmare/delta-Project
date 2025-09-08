cd485f34aa03c2ba167d1957a954f43e749e02ef
shreyas halmare <halmareshreyash654@gmail.com>
2025-09-08 15:03:02 +0530

    Initial commit

---
+++ models/listing.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        url: String,
+        filename: String,
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const listing = mongoose.model("listing", listingSchema);
+module.exports = listing;
